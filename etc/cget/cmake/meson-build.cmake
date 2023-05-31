dfe9114489294c9ef76654b71389facdbe90bf28
Jörg Walter <joerg.walter@offis.de>
2023-05-31 12:37:01 +0200

    add license specification

--- etc/cget/cmake/meson-build.cmake
+++ etc/cget/cmake/meson-build.cmake
@@ -1,3 +1,15 @@
+#********************************************************************************
+# Copyright (c) 2018, 2023 OFFIS e.V.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+# 
+# Contributors:
+#    Jörg Walter - initial implementation
+# *******************************************************************************/
 ##############################################################################
 #
 # CMake script to build meson-based packages correctly
