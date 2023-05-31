fb55bf3c3dce00d0f911f81cb274480a86bc8b3a
Jörg Walter <joerg.walter@offis.de>
2023-05-31 12:37:01 +0200

    add license specification

--- etc/cget/cget.sh
+++ etc/cget/cget.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#********************************************************************************
+# Copyright (c) 2018, 2023 OFFIS e.V.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+# 
+# Contributors:
+#    Jörg Walter - initial implementation
+# *******************************************************************************/
 #
 # This is a shell-only re-implementation of https://github.com/pfultz2/cget .
 # Its main difference are improved self-contained portability of cget prefixes
--- etc/cget/cmake/toolchain-utils.cmake
+++ etc/cget/cmake/toolchain-utils.cmake
@@ -1,3 +1,15 @@
+#********************************************************************************
+# Copyright (c) 2018, 2023 OFFIS e.V.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+# 
+# Contributors:
+#    Jörg Walter - initial implementation
+# *******************************************************************************/
 ###############################################################################
 # this needs a project() command that matches the recipe subdirectory name
 set(CGET_RECIPE_DIR ${CGET_PREFIX}/etc/cget/recipes/${CMAKE_PROJECT_NAME})
