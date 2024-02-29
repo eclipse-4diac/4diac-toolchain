89c0ef081a01f0d0511871f320aa9bc3448fb8a8
Jörg Walter <joerg.walter@offis.de>
2024-02-29 11:58:03 +0100

    prepare on-demand download/installation of cross compilers

--- etc/install-crosscompiler.sh
+++ etc/install-crosscompiler.sh
@@ -15,10 +15,27 @@ set -e
 
 cd "$(dirname "$0")/.."
 
+hostarch="$(uname -m)" # x86_64
+hostplatform="$(uname -s)" # Linux
+[ "$hostplatform" = "Windows_NT" ] && hostplatform=Windows
+fileprefix="${hostplatform}-cross-${hostarch}_"
+
 mkdir -p .cache
-for i in *.tar.lz; do
-        [ -f "$i" -a ! -d "${i%.tar.lz}" ] || continue
-        echo "Adding toolchain package $i"
-        bin/lzip -d < "$i" | bin/tar x
-        mv "$i" .cache/
+mv "$fileprefix"*.tar.lz .cache 2>/dev/null || true
+
+if [ -z "$*" ]; then
+        echo "No toolchain installed. Call $0 <target-arch> to install one. Local cache contains:"
+        ls ".cache/$fileprefix"*.tar.lz | sed -e "s,^.cache/$fileprefix,,;s,\\.tar\\.lz\$,,"
+        exit 0
+fi
+
+for targetarch in "$@"; do
+        [ ! -f "$targetarch.cmake" ] || continue
+        if [ ! -f ".cache/$fileprefix$targetarch.tar.lz" ]; then
+                echo "No locally cached toolchain for $targetarch found. Please download"
+                echo "$fileprefix$targetarch.tar.lz from wherever you downloaded this package"
+                exit 1
+        fi
+        echo "Adding toolchain for target $targetarch"
+        bin/lzip -d < ".cache/$fileprefix$targetarch.tar.lz" | bin/tar x
 done
